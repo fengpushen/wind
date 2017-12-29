d1a0a5933390229f065990c0519f651c43380aa1
fengpushen <fengpushen@sina.com>
2017-12-29 16:58:52 +0800

    a

--- windFrame/db.sql
+++ windFrame/db.sql
@@ -1,26 +1,3 @@
-insert into com_area
-  (area_code,
-   area_name,
-   pcode,
-   province_code,
-   city_code,
-   country_code,
-   street_code,
-   village_code,
-   area_level)
-  select org_code,
-         org_name,
-         org_up_code,
-         s_code,
-         sz_code,
-         qx_code,
-         jd_code,
-         sq_code,
-         org_level
-    from hn_org
-   where length(org_code) = 12
-/
----------------------------------------------------------------
 drop table bs_hr_label
 /
 -- Create table
