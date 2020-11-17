a91f214796b06e85eecf0f9d30bcf7467b859111
fengpushen <fengpushen@sina.com>
2020-11-17 17:13:45 +0800

    aaa

--- windFrame/db.sql
+++ windFrame/db.sql
@@ -1,44 +1,12 @@
---v_bs_position、v_valid_bs_position
-
-select area_code,
-       count(1) as ccount,
-       nvl(sum(c.pcount), 0) as pcount,
-       nvl(sum(c.p_num), 0) as p_num,
-       nvl(sum(d.pcount_valid), 0) as pcount_valid,
-       nvl(sum(d.p_num_valid), 0) as p_num_valid
-  from BS_COMPANY a
- inner join (select frame_account.account_id, busi_staff.area_code
-               from frame_account
-              inner join busi_staff
-                 on busi_id = staff_id) b
-    on a.opr_id = b.account_id
-  left outer join (select c_id, count(1) as pcount, sum(p_num) as p_num
-                     from BS_POSITION
-                    group by c_id) c
-    on a.c_id = c.c_id
-  left outer join (select c_id,
-                          count(1) as pcount_valid,
-                          sum(p_num) as p_num_valid
-                     from v_valid_bs_position
-                    group by c_id) d
-    on a.c_id = d.c_id
- group by area_code
- 
------------------------------20201105
-COM_ATTA
-BS_JCPTPX
-BX_JCPTCP
-BS_PXXM
-BS_JCPTPX_XM
-BX_JCPTCP_XM
-BX_JCPTCP_XM_ATTA
-
-v_BX_JCPTCP_XM_ATTA
-v_BX_JCPTCP
-v_com_atta
-v_bs_pxxm
-SEQ_UNIQUE
-f_make_unique_id
-
-frame_config
-file_root\
+drop table bak_frame_menu_tree
+/
+create table bak_frame_menu_tree as select * from frame_menu_tree
+/
+drop table bak_frame_role
+/
+create table bak_frame_role as select * from frame_role
+/
+drop table bak_frame_role_menu
+/
+create table bak_frame_role_menu as select * from frame_role_menu
+/
